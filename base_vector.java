6b133efd386054ebe77d6d98caab64b90b452211
ZhangYang <1944952458@qq.com>
2016-10-23 16:38:14 +0800

    初始化基向量
    3的倍数的基向量初始化

--- base_vector.java
+++ base_vector.java
@@ -1,5 +1,23 @@
 package base3times16;
-
 public class base_vector {
+	public static int[][] vector(int n) {
+		int[] base = new int[n];
+		/**
+		 * 以下初始化基向量
+		 */
+		int[][] baseVector = new int[(int)(Math.pow(2, n+2)-1)][n];
+		for(int i=0; i<base.length;i++){
+			base[i] = 3*i;
+		}
+		base[0] = 1;
+		/**
+		 * 以下求基向量对应的权值向量
+		 */
+		return baseVector;
+		
+	}
+	public static void main(String[] args) {
+		vector(3);
+	}
 
 }
