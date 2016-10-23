82b4e13b01a13ee31a5ffdaec8e995a1d46933c0
ZhangYang <1944952458@qq.com>
2016-10-23 16:37:10 +0800

    Revert "基向量初始化"
    This reverts commit ebfd76dc502f9e53360448680768a40dc71fc3ab.

--- main_embed.java
+++ main_embed.java
@@ -1,10 +1,10 @@
 package base3times16;
 import notContinue.scrProcess;
 public class main_embed {
-   public static int[][] embed16(int[][] oriArr,int n) throws Exception{
+   public static int[][] embed16(int[][] oriArr,int n){
 		int[][] sctArr = new int[256][256];
-		int[][] scrSqc = scrProcess.srt(2*n+1);
-		return sctArr;
+		int scrSqc = scrProcess.srt(2*n+1)
+		return srtArr;
 	} 
 
 }
