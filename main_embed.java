9d6a555b38447c5201684734565e2f8fec4eba53
ZhangYang <1944952458@qq.com>
2016-10-23 13:48:46 +0800

    秘密信息序列
    从其他文件导入秘密信息生成序列

---
+++ main_embed.java
@@ -0,0 +1,10 @@
+package base3times16;
+import notContinue.scrProcess;
+public class main_embed {
+   public static int[][] embed16(int[][] oriArr,int n){
+		int[][] sctArr = new int[256][256];
+		int scrSqc = scrProcess.srt(2*n+1)
+		return srtArr;
+	} 
+
+}
